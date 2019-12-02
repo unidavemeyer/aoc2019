3a00ce56679ab9d33115c16d9d2478f42ea19088
Dave Meyer <unidavemeyer@gmail.com>
2019-12-01 17:18:15 -0800

    More basics
    I have enough bits and pieces that I'm going to try the first day's
    problem, and figure I can learn additional stuff as I need it after
    that.

--- Basics.java
+++ Basics.java
@@ -10,5 +10,27 @@ class Basics
 	public static void main(String[] args)
 	{
 		System.out.println("Testing, 1, 2, 3...");
+
+		int nValue = 12345;
+		int nValueHex = 0x12345;
+		int nValueBin = 0b10101;
+
+		long nLong = 1234567890L;
+		long nLong2 = 1234_5678_90L;	// underscores are allowed between groups just for kicks
+
+		boolean fValue = true;
+
+		int[] aN;
+		aN = new int[2];
+		aN[0] = 12;
+		aN[1] = 34;
+
+		int[] aNAlt = { 0, 1, 2, 3, };
+
+		System.out.println("Array has " + aNAlt.length + " elements");
+
+		String str = "a b c d";
+
+		System.out.println("String is a string? " + (str instanceof String));
 	}
 }
