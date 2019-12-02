ef9b8648d08b372e883ce00c35b1b96f76f0aba2
Dave Meyer <unidavemeyer@gmail.com>
2019-12-01 16:54:51 -0800

    Very beginnings
    Started a basics java class to experiment with a few things. Set up a
    reasonable .gitignore for now, and updated the README to reflect the
    current state of things.

---
+++ Basics.java
@@ -0,0 +1,14 @@
+// Super simple basic set of stuff to help refresh my memory on how to write Java code
+
+/** Comments that start with double star are documentation ones.
+ *
+ * This class hold some of the basics for making things go.
+ */
+
+class Basics
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Testing, 1, 2, 3...");
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aoc2019
 Advent of Code solutions for 2019
+
+The plan this year is to refresh my Java knowledge. The last few years I have done things
+in python, which I really enjoy, but I need to keep my mind fresh and active. :)
