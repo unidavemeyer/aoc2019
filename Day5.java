0fff66f234e9cd4e33cae987dadf51d12e01df96
Dave Meyer <unidavemeyer@gmail.com>
2019-12-05 21:25:40 -0800

    Progress on day5a
    I've rebuilt much of the interpreter now as a static nested class, so
    that it can do its own stuff, track state, etc. Not completely convinced
    I've done everything properly there, but the basic idea is sound, I
    think.

--- Day5.java
+++ Day5.java
@@ -1,9 +1,11 @@
-// Day 2 solution
+// Day 5 solution
 
-/** Frontend driver for day 2
+/** Frontend driver for day 5
  */
-class Day2
+class Day5
 {
+	// Op stream from day 2
+
 	static int s_aNOp1[] = { 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,5,19,23,1,23,6,27,1,5,27,31,1,31,6,35,1,
 				9,35,39,2,10,39,43,1,43,6,47,2,6,47,51,1,5,51,55,1,55,13,59,1,59,10,63,2,10,63,67,1,
 				9,67,71,2,6,71,75,1,5,75,79,2,79,13,83,1,83,5,87,1,87,9,91,1,5,91,95,1,5,95,99,1,99,
@@ -21,17 +23,27 @@ class Day2
 
 	static void Part1()
 	{
+	}
+
+	static void Part2()
+	{
+	}
+
+	static void Part1Day2()
+	{
 		int[] aNOp = java.util.Arrays.copyOf(s_aNOp1, s_aNOp1.length);
 
 		aNOp[1] = 12;
 		aNOp[2] = 2;
 
-		RunOps(aNOp);
+		Interp interp = new Interp();
+		interp.SetOps(aNOp);
+		interp.RunOps();
 		
-		System.out.println("Result: " + aNOp[0]);
+		System.out.println("Result: " + interp.GetOp(0));
 	}
 
-	static void Part2()
+	static void Part2Day2()
 	{
 		int goal = 19690720;
 
@@ -45,9 +57,11 @@ class Day2
 				aNOp[1] = noun;
 				aNOp[2] = verb;
 
-				RunOps(aNOp);
+				Interp interp = new Interp();
+				interp.SetOps(aNOp);
+				interp.RunOps();
 
-				if (aNOp[0] == goal)
+				if (interp.GetOp(0) == goal)
 				{
 					System.out.println("Found goal with noun " + noun + " and verb " + verb + " for total " + (100 * noun + verb));
 					break nouns;
@@ -58,51 +72,137 @@ class Day2
 		System.out.println("If we got here without a result, too bad!");
 	}
 
-	static void RunOps(int[] aNOp)
+	static class Interp
 	{
-		int iOp = 0;
-		while (iOp < aNOp.length)
-		{
-			switch (aNOp[iOp])
-			{
-			case 1:
-				RunAdd(aNOp, iOp);
-				break;
-			case 2:
-				RunMultiply(aNOp, iOp);
-				break;
-			case 99:
-				return;
-			default:
-				System.out.println("Oops!");
-				return;
-			}
+		int m_iOp = 0;
+		int[] m_aNOp = null;
 
-			iOp += 4;
+		int m_iNInput = 0;
+		int[] m_aNInput = null;
+
+		public void SetInput(int[] aNInput)
+		{
+			m_aNInput = aNInput;
 		}
 
-		System.out.println("Also oops!");
-	}
+		public void SetOps(int[] aNOp)
+		{
+			m_aNOp = aNOp;
+		}
 
-	static void RunAdd(int[] aNOp, int iOp)
-	{
-		int iA = aNOp[iOp + 1];
-		int a = aNOp[iA];
-		int iB = aNOp[iOp + 2];
-		int b = aNOp[iB];
-		int iOpDst = aNOp[iOp + 3];
-		// System.out.println("Adding: " + a + " + " + b + " -> " + iOpDst);
-		aNOp[iOpDst] = a + b;
-	}
+		public int GetOp(int iOp)
+		{
+			return m_aNOp[iOp];
+		}
 
-	static void RunMultiply(int[] aNOp, int iOp)
-	{
-		int iA = aNOp[iOp + 1];
-		int a = aNOp[iA];
-		int iB = aNOp[iOp + 2];
-		int b = aNOp[iB];
-		int iOpDst = aNOp[iOp + 3];
-		// System.out.println("Multiplying: " + a + " * " + b + " -> " + iOpDst);
-		aNOp[iOpDst] = a * b;
+		public void RunOps()
+		{
+			m_iOp = 0;
+			m_iNInput = 0;
+
+			while (m_iOp < m_aNOp.length)
+			{
+				switch (m_aNOp[m_iOp] % 100)
+				{
+				case 1:
+					RunAdd();
+					break;
+				case 2:
+					RunMultiply();
+					break;
+				case 3:
+					RunInput();
+					break;
+				case 4:
+					RunOutput();
+					break;
+				case 99:
+					return;
+				default:
+					System.out.println("Oops!");
+					return;
+				}
+			}
+
+			System.out.println("Also oops!");
+		}
+
+		int[] ANMode(int op, int cMode)
+		{
+			int[] aNMode = new int[cMode];
+
+			op /= 100;
+
+			while (cMode > 0)
+			{
+				--cMode;
+				aNMode[cMode] = op % 10;
+				op /= 10;
+			}
+
+			return aNMode;
+		}
+
+		void RunAdd()
+		{
+			int[] aNMode = ANMode(m_aNOp[m_iOp], 2);
+
+			int iA = m_aNOp[m_iOp + 1];
+			int a = (aNMode[0] == 0) ? m_aNOp[iA] : iA;
+			int iB = m_aNOp[m_iOp + 2];
+			int b = (aNMode[1] == 0) ? m_aNOp[iB] : iB;
+
+			int iOpDst = m_aNOp[m_iOp + 3];
+
+			// System.out.println("Adding: " + a + " + " + b + " -> " + iOpDst);
+			m_aNOp[iOpDst] = a + b;
+
+			m_iOp += 4;
+		}
+
+		void RunMultiply()
+		{
+			int[] aNMode = ANMode(m_aNOp[m_iOp], 2);
+
+			int iA = m_aNOp[m_iOp + 1];
+			int a = (aNMode[0] == 0) ? m_aNOp[iA] : iA;
+			int iB = m_aNOp[m_iOp + 2];
+			int b = (aNMode[1] == 0) ? m_aNOp[iB] : iB;
+
+			int iOpDst = m_aNOp[m_iOp + 3];
+
+			// System.out.println("Multiplying: " + a + " * " + b + " -> " + iOpDst);
+			m_aNOp[iOpDst] = a * b;
+
+			m_iOp += 4;
+		}
+
+		void GetInput(int iAddr)
+		{
+			m_aNOp[iAddr] = m_aNInput[m_iNInput++];
+		}
+
+		void RunInput()
+		{
+			int[] aNMode = ANMode(m_aNOp[m_iOp], 1);
+			assert(aNMode[0] == 0);
+
+			int iAddr = m_aNOp[m_iOp + 1];
+			GetInput(iAddr);
+
+			m_iOp += 2;
+		}
+
+		void RunOutput()
+		{
+			int[] aNMode = ANMode(m_aNOp[m_iOp], 1);
+
+			int iAddr = m_aNOp[m_iOp + 1];
+			int nValue = (aNMode[0] == 0) ? m_aNOp[iAddr] : iAddr;
+
+			System.out.println("op " + m_iOp + " [" + iAddr + "] = " + nValue);
+
+			m_iOp += 2;
+		}
 	}
 }
