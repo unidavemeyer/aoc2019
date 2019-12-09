0ea5ce4215989454d8d4e894f3d08c7a7c3d67e3
Dave Meyer <unidavemeyer@gmail.com>
2019-12-08 16:55:04 -0800

    Day 7, part 1 now done
    The walk-all-permutations code is super gross, but it got the job done.
    I feel like there is actually some kind of closed form thing that I
    could/should have been using there rather than the recursive goo that I
    threw at it, but I cannot recall or invent how to do that right now.

--- Day7.java
+++ Day7.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 
 /** Day7 solutions
  */
@@ -7,8 +8,9 @@ class Day7
 	{
 		// Example1();
 		// Example2();
-		
-		Example3();
+		// Example3();
+
+		Part1();
 	}
 
 	static void Example1()
@@ -33,6 +35,52 @@ class Day7
 		int[] aNProgram = { 3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0 };
 
 		System.out.println("Example1 output: " + NRunAmps(aNProgram, aNPhase, 0));
+	}
+
+	static void Part1()
+	{
+		int[] aNProgram = {3,8,1001,8,10,8,105,1,0,0,21,46,67,76,97,118,199,280,361,442,99999,3,9,1002,9,3,9,101,4,9,9,102,3,9,9,1001,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,101,5,9,9,1002,9,2,9,101,2,9,9,4,9,99,3,9,101,4,9,9,4,9,99,3,9,1001,9,4,9,102,2,9,9,1001,9,4,9,1002,9,5,9,4,9,99,3,9,102,3,9,9,1001,9,2,9,1002,9,3,9,1001,9,3,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99};
+		int[] aNPhaseAll = { 0,1,2,3,4 };
+		int[] aNPhase = new int[5];
+
+		HashSet<Integer> setPhase = new HashSet<>();
+		for (int nPhase : aNPhaseAll)
+		{
+			setPhase.add(nPhase);
+		}
+
+		System.out.println("Maximal output: " + NMaximalAmps(aNProgram, setPhase, aNPhase, 0, 0));
+	}
+
+	static int NMaximalAmps(int[] aNProgram, HashSet<Integer> setPhase, int[] aNPhase, int iPhase, int nInit)
+	{
+		int nBest = -1;	// BB (davidm) sufficient?
+
+		for (int nPhaseCur : setPhase)
+		{
+			aNPhase[iPhase] = nPhaseCur;
+
+			HashSet<Integer> setPhaseCur = new HashSet<>();
+			setPhaseCur.addAll(setPhase);
+			setPhaseCur.remove(nPhaseCur);
+
+			int nCur;
+			if (iPhase < aNPhase.length - 1)
+			{
+				nCur = NMaximalAmps(aNProgram, setPhaseCur, aNPhase, iPhase + 1, nInit);
+			}
+			else
+			{
+				nCur = NRunAmps(aNProgram, aNPhase, nInit);
+			}
+
+			if (nCur > nBest)
+			{
+				nBest = nCur;
+			}
+		}
+
+		return nBest;
 	}
 
 	static int NRunAmps(int[] aNProgram, int[] aNPhase, int nInit)
